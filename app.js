fa3c43b10950a0317bb1c9a4df44981991b025fa
chicadan <danielapatane@MacBook-Pro-de-Daniela-2.local>
2022-10-15 18:33:26 -0300

    union css, nav y footer

---
+++ app.js
@@ -0,0 +1,25 @@
+//FIERRO
+    //BOTON
+
+    document.getElementById ("buttom-up").addEventListener("click", scrollUp);
+
+function scrollUp (){
+   
+    var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+        
+    if (currentScroll > 0 ) {
+        window.requestAnimationFrame(scrollUp);
+        window.scrollTo (0 , currentScroll - (currentScroll/10) )
+        }
+}
+
+buttomUp = document.getElementById("buttom-up");
+    window.onscroll = function(){
+    var scroll = document.documentElement.scrollTop;
+        if (scroll > 500) {
+            buttomUp.style.transform = "scale(1)"
+        } else if (scroll<500){
+            buttomUp.style.transform = "scale(0)"
+        }
+}
+
