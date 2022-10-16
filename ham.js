c19749facac0a02ab67f2b3c8dc8e0bd85d9f859
dougojeda <112479214+dougojeda@users.noreply.github.com>
2022-10-16 13:10:26 -0300

    menu hamburgesa y responsive solo en index

---
+++ ham.js
@@ -0,0 +1,12 @@
+// const navToggle = document.querySelector(".nav-toggle");
+// const navMenu = document.querySelector(".nav-menu");
+
+const navToggle = document.querySelector(".nav-toggle");
+
+const navMenu = document.querySelector(".menu");
+
+navToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("nav-menu_visible");
+
+  
+});
